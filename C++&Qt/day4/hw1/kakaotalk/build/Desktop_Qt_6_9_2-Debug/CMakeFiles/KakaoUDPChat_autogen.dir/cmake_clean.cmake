2888d69fdb06e8042f3b2fb706a8bf6bdb45a408
Konbu-kw <sion05163@gmail.com>
2025-09-19 21:24:43 +0900

    Delete C++&Qt/day4/hw1/kakaotalk/build/Desktop_Qt_6_9_2-Debug directory

--- C++&Qt/day4/hw1/kakaotalk/build/Desktop_Qt_6_9_2-Debug/CMakeFiles/KakaoUDPChat_autogen.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/KakaoUDPChat_autogen"
-  "KakaoUDPChat_autogen/mocs_compilation.cpp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/KakaoUDPChat_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
